db78c70b78d8d255342899f9d5c0e1f253e1d5ce
ibtissam <gaamouibtissam@gmail.com>
2025-05-08 11:48:05 +0100

    commencer auth

--- model/database/connexiondb.php
+++ model/database/connexiondb.php
@@ -10,16 +10,18 @@ class Database {
         $this->conn = null;
 
         try {
-            // Connexion PDO
+            // Connexion PDO avec gestion des erreurs
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
                                   $this->username, 
                                   $this->password);
             $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Active le mode exception pour gérer les erreurs PDO
         } catch (PDOException $exception) {
-            echo "Erreur de connexion : " . $exception->getMessage();
+            echo "Erreur de connexion : " . $exception->getMessage(); // Affiche l'erreur détaillée si une exception se produit
         }
 
         return $this->conn;
     }
 }
+
 ?>
