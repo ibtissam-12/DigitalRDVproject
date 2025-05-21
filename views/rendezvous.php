5b4c8b6fb19bd549a77256aa0ddefb711c05f5e5
ibtissam <gaamouibtissam@gmail.com>
2025-05-21 22:57:08 +0100

    version

--- views/rendezvous.php
+++ views/rendezvous.php
@@ -103,6 +103,7 @@ if (isset($_POST['date'])) {
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+     <link rel="icon" href="images/logo.png" type="image/gif" />
     <style>
         body {
             font-family: 'Poppins', Arial, sans-serif;
